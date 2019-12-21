e59b47bbe675adfc39db2f33b27f0415c79cc0f1
Muhammad Fathur Rahman <cyberflyx@gmail.com>
2019-12-21 23:36:53 +0800

    feat: set parent TestCase class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,25 @@
 
 namespace Tests;
 
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use WithFaker;
+    use RefreshDatabase;
+
+    protected $admin_user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->admin_user = factory(User::class)->make([
+            'type' => 'a',
+        ]);
+    }
 }
