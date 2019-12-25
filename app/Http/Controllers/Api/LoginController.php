c5c4920e6306e917f3f8a3c1f1f15cfb8aa2259c
Muhammad Fathur Rahman <cyberflyx@gmail.com>
2019-12-25 10:17:43 +0800

    feat: Login / Logout / Me api endpoint

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Resources\UserResource;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+
+class LoginController extends ApiController
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Override method
+     * Send the response after the user was authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return UserResource
+     */
+    protected function sendLoginResponse(Request $request)
+    {
+        $user = $this->guard()->user();
+
+        $token = $user->createToken('Laravel Personal Access Client')->accessToken;
+
+        $logged_in_time = now();
+
+        return (new UserResource($user))->additional(['token' => $token, 'logged_in_time' => $logged_in_time]);
+    }
+
+    /**
+     * Override method
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    /**
+     * Get current auth user info
+     * @return UserResource
+     */
+    public function me()
+    {
+        $user = $this->guard()->user();
+
+        return new UserResource($user);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\JsonMiddleware;
 use Illuminate\Http\Request;
 
 /*
@@ -15,4 +16,25 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::namespace('Api')->middleware(JsonMiddleware::class)->group(function () {
+
+    // Login routes
+    Route::post('login', 'LoginController@login');
+
+    // authenticated routes
+
+    Route::middleware('auth:api')->group(function () {
+
+        // Log out
+
+        Route::post('logout', 'LoginController@logout');
+
+        // current user info
+
+        Route::get('me', 'LoginController@me');
+
+    });
+
 });
