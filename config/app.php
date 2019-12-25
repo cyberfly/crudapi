494183a1a851a72e3a1c48c5a978882515af5dac
Muhammad Fathur Rahman <cyberflyx@gmail.com>
2019-12-25 11:54:17 +0800

    chore: new macro result()

--- config/app.php
+++ config/app.php
@@ -175,6 +175,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+        * Macros Service Providers...
+        */
+        \App\Providers\BuilderServiceProvider::class
+
     ],
 
     /*
