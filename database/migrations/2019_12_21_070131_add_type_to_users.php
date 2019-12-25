a6c915b51d1215319fac43165e9c75686b149e4e
Muhammad Fathur Rahman <cyberflyx@gmail.com>
2019-12-25 16:38:50 +0800

    fix: need to put default value to prevent error when phpunit test with sqlite

--- database/migrations/2019_12_21_070131_add_type_to_users.php
+++ database/migrations/2019_12_21_070131_add_type_to_users.php
@@ -14,7 +14,7 @@ class AddTypeToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->char('type')->after('email');
+            $table->char('type')->after('email')->default('u');
         });
     }
 
